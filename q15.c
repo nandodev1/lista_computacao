8ca708361c90329c8e73cb58283e5219fc75a8ad
Fernando Gomes <nandobink6@gmail.com>
2020-07-29 21:51:13 +0000

    q14 e q15 comcluida

---
+++ q15.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int count = 0;
+  int i;
+  for(i = 1; i <= 200; i++){
+    if((i % 2) != 0)count += 1;
+  }
+  printf("Quantidade de ínpares %d\n", count);
+}
